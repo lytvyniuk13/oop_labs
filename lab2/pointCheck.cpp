7476a29a6bca351d65d3397b69302be30917d43a
lytvyniuk <yaroslav.lytvyniuk@lnu.edu.ua>
2026-04-13 21:45:27 +0300

    upd

--- lab2/pointCheck.cpp
+++ lab2/pointCheck.cpp
@@ -13,8 +13,45 @@ double vectorProduct(point a, point b, point p) {
     return (b.x - a.x) * (p.y - a.y) - (b.y - a.y) * (p.x - a.x);
 }
 
-bool isInside(triangle t, point p, bool &isOnEdge) {
+double dist(point p1, point p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+double heronArea(point p1, point p2, point p3) {
+    double a = dist(p1, p2);
+    double b = dist(p2, p3);
+    double c = dist(p3, p1);
+    double s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+bool isInside_v(triangle t, point p, bool &isOnEdge) {
     isOnEdge = false;
+    
+    if (isTriangleAPoint(t)) {
+        if (t.a.x == p.x && t.a.y == p.y) {
+            isOnEdge = true;
+        }
+        return false;
+    }
+
+    if (isDegenerate(t)) {
+        double d1 = dist(t.a, t.b);
+        double d2 = dist(t.b, t.c);
+        double d3 = dist(t.c, t.a);
+
+        if (d2 >= d1 && d2 >= d3) {
+            t.a = t.b; t.b = t.c;
+        } else if (d3 >= d1 && d3 >= d2) {
+            t.a = t.a; t.b = t.c;
+        }
+        
+        double scalar = ((p.x - t.a.x) * (t.b.x - t.a.x) + (p.y - t.a.y) * (t.b.y - t.a.y)) / 
+                        ((t.b.x - t.a.x) * (t.b.x - t.a.x) + (t.b.y - t.a.y) * (t.b.y - t.a.y));
+        if (vectorProduct(t.a, t.b, p) != 0) return false;
+        if (scalar < 0 || scalar > 1) return false;
+        else isOnEdge = true; return false;
+    }
+
     double v1 = vectorProduct(t.a, t.b, p);
     double v2 = vectorProduct(t.b, t.c, p);
     double v3 = vectorProduct(t.c, t.a, p);
@@ -25,10 +62,66 @@ bool isInside(triangle t, point p, bool &isOnEdge) {
         if (!(hasPos && hasNeg)) {
             isOnEdge = true;
         }
+        
         return false;
     }
     return (v1 > 0 && v2 > 0 && v3 > 0) || (v1 < 0 && v2 < 0 && v3 < 0);
 }
+
+bool isInside_h(triangle t, point p, bool &isOnEdge) {
+    isOnEdge = false;
+    
+    if (isTriangleAPoint(t)) {
+        double dx = t.a.x - p.x;
+        double dy = t.a.y - p.y;
+        if ((dx < 0 ? -dx : dx) < 1e-7 && (dy < 0 ? -dy : dy) < 1e-7) {
+            isOnEdge = true;
+        }
+        return false;
+    }
+    
+    if (isDegenerate(t)) {
+        double d1 = dist(t.a, t.b);
+        double d2 = dist(t.b, t.c);
+        double d3 = dist(t.c, t.a);
+
+        if (d2 >= d1 && d2 >= d3) {
+            t.a = t.b; t.b = t.c;
+        } else if (d3 >= d1 && d3 >= d2) {
+            t.a = t.a; t.b = t.c;
+        }
+        
+        double scalar = ((p.x - t.a.x) * (t.b.x - t.a.x) + (p.y - t.a.y) * (t.b.y - t.a.y)) / 
+                        ((t.b.x - t.a.x) * (t.b.x - t.a.x) + (t.b.y - t.a.y) * (t.b.y - t.a.y));
+        if (vectorProduct(t.a, t.b, p) != 0) return false;
+        if (scalar < 0 || scalar > 1) return false;
+        else isOnEdge = true; return false;
+    }
+
+double totalArea = heronArea(t.a, t.b, t.c);
+    double A1 = heronArea(p, t.a, t.b);
+    double A2 = heronArea(p, t.b, t.c);
+    double A3 = heronArea(p, t.c, t.a);
+
+    double sumArea = A1 + A2 + A3;
+    double diff = totalArea - sumArea;
+
+    if ((diff < 0 ? -diff : diff) > 0) return false;
+    if (A1 == 0 || A2 == 0 || A3 == 0) {
+        isOnEdge = true;
+        return false;
+    }
+    
+    return true;
+}
+
+void printStatus(bool inside, bool onEdge) {
+    if (inside) cout << "inside";
+    else if (onEdge) cout << "on the edge";
+    else cout << "outside";
+    cout << endl;
+}
+
 int displayProgram(){
 bool isOnEdge = false;
 triangle t;
@@ -41,12 +134,11 @@ if(!(cin >> t.a.x >> t.a.y >> t.b.x >> t.b.y >> t.c.x >> t.c.y)) {
 
 if (isTriangleAPoint(t)) {
     cout << "The triangle is a point." << endl;
-    return 0;
 }
-if (isDegenerate(t)) {
+else if (isDegenerate(t)) {
     cout << "The triangle is degenerate (a line)." << endl;
-    return 0;
 }
+
 cout << "Enter the amount of points to check: ";
 int n;
 if(!(cin >> n)) {
@@ -61,16 +153,15 @@ if(!(cin >> p.x >> p.y)) {
     cerr << "Error reading point coordinates." << endl;
     return 1;
 }
-if (isInside(t, p, isOnEdge)) {
-        cout << "The point is inside the triangle." << endl;
-    } else {
-            if (isOnEdge) {
-                cout << "The point is on the edge of the triangle." << endl;
-                isOnEdge = false;
-            } else {
-                cout << "The point is outside the triangle." << endl;
-            }
-    }
+    
+    bool insideVector = isInside_v(t, p, isOnEdge);
+
+    bool isOnEdgeHeron = false;
+    bool insideHeron = isInside_h(t, p, isOnEdgeHeron);
+
+    cout << "VECTOR: "; printStatus(insideVector, isOnEdge);
+    cout << "HERON: "; printStatus(insideHeron, isOnEdgeHeron);
+
 }
 return 0;
 }
--- lab2/pointCheck.h
+++ lab2/pointCheck.h
@@ -11,6 +11,13 @@ struct triangle {
 bool isDegenerate(triangle t);
 bool isTriangleAPoint(triangle t);
 double vectorProduct(point a, point b, point p);
-bool isInside(triangle t, point p, bool &isOnEdge);
+
+double dist(point p1, point p2);
+double heronArea(point p1, point p2, point p3);
+
+void printStatus(bool inside, bool onEdge); 
+
+bool isInside_v(triangle t, point p, bool &isOnEdge);
+bool isInside_h(triangle t, point p, bool &isOnEdge);
 int displayProgram();
 #endif
